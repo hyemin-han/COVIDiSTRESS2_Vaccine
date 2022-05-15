302ff253db35a78315c8584b370b066c374ad0fc
Hyemin Han <hyemin.han@ua.edu>
2022-05-15 09:55:18 -0500

    Additional variables (consp, anti) alignment

--- Vaccine_MI.R
+++ Vaccine_MI.R
@@ -78,7 +78,65 @@ mod1.vx$es.invariance['R2',]
 #loadings intercepts 
 # 0.9675333  0.9968223    good
 
+#####
+# 2. Conspiratorial
+item.consp <- c('conspirational_think_1','conspirational_think_2',
+               'conspirational_think_3','conspirational_think_4')
+cfa.consp<-'
+CONSP =~ conspirational_think_1 + conspirational_think_2+
+  conspirational_think_3 + conspirational_think_4'
 
+# configural invariance
+# DWLS preferred with 5+ categories (Christine DiStefano & Grant B. Morgan 2014)
+fit.consp <- cfa(cfa.consp, data.mi, estimator = 'WLSMV', group='UserLanguage')
+# fit indices
+fitMeasures(fit.consp)[fits]
+
+# no good rmsea -> alignment
+#rmsea.scaled         srmr   cfi.scaled   tli.scaled 
+#0.15744704   0.03697213   0.94147075   0.82441225 
+
+# measurement alignment test
+# extract parameters
+par.consp <- invariance_alignment_cfa_config(dat = data.mi[,item.consp], 
+                                          group = data.mi$UserLanguage)
+# do alignment
+mod1.consp <- invariance.alignment(lambda = par.consp$lambda, nu =
+                                  par.consp$nu, align.scale = c(0.2, 0.4), align.pow = c(0.25, 0.25))
+# test performance
+mod1.consp$es.invariance['R2',]
+#loadings intercepts 
+# 0.9705771  0.9860852    good
+
+#####
+# 3. Antiexpert
+items.anti <- c('antiexpert_1','antiexpert_2','antiexpert_3')
+cfa.model.anti <- 'ANTI =~ antiexpert_1 + antiexpert_2+antiexpert_3'
+cfa.whole.anti <- cfa(model=cfa.model.anti,data=data.mi,estimator='WLSMV', group = 
+                      'UserLanguage')
+fitMeasures(cfa.whole.anti)[fits]
+# msea.scaled         srmr   cfi.scaled   tli.scaled 
+#    0.08972321   0.02870422   0.93010890   0.86021779 
+
+# good. metric?
+cfa.metric.anti <- cfa(model=cfa.model.anti,data=data.mi,estimator='WLSMV', group = 
+                         'UserLanguage', group.equal='loadings')
+fitMeasures(cfa.metric.anti)[fits]
+# not good. MA
+#rmsea.scaled         srmr   cfi.scaled   tli.scaled 
+#0.09410097   0.04009779   0.93973035   0.90624721
+
+# measurement alignment test
+# extract parameters
+par.anti <- invariance_alignment_cfa_config(dat = data.mi[,items.anti], 
+                                          group = data.mi$UserLanguage)
+# do alignment
+mod1.anti <- invariance.alignment(lambda = par.anti$lambda, nu =
+                                  par.anti$nu, align.scale = c(0.2, 0.4), align.pow = c(0.25, 0.25))
+# test performance
+mod1.anti$es.invariance['R2',]
+#loadings intercepts 
+# 0.8592692  0.9875114     good
 
 #####
 # factor score calculation
@@ -91,12 +149,12 @@ for (i in 1:length(langs.include)){
     F.vx <- aligned.factor.scores(mod1.vx$lambda.aligned[i,],
                                mod1.vx$nu.aligned[i,],
                                data.mi[data.mi$UserLanguage==langs.include[i],vaxs])
-#    F.sps <- aligned.factor.scores(mod1.sps$lambda.aligned[i,],
-#                                   mod1.sps$nu.aligned[i,],
-#                                   data.mi[data.mi$UserLanguage==langs.include[i],items.sps])
-#    F.id <- aligned.factor.scores(mod1.identity$lambda.aligned[i,],
-#                                  mod1.identity$nu.aligned[i,],
-#                                   data.mi[data.mi$UserLanguage==langs.include[i],items.identity])
+    F.consp <- aligned.factor.scores(mod1.consp$lambda.aligned[i,],
+                                   mod1.consp$nu.aligned[i,],
+                                   data.mi[data.mi$UserLanguage==langs.include[i],item.consp])
+    F.anti <- aligned.factor.scores(mod1.anti$lambda.aligned[i,],
+                                 mod1.anti$nu.aligned[i,],
+                                   data.mi[data.mi$UserLanguage==langs.include[i],items.anti])
 #    F.rs <- aligned.factor.scores(mod1.resilience$lambda.aligned[i,],
 #                                  mod1.resilience$nu.aligned[i,],
 #                                  data.mi[data.mi$UserLanguage==langs.include[i],items.resilience])
@@ -104,8 +162,8 @@ for (i in 1:length(langs.include)){
 #                                  mod1.ps$nu.aligned[i,],
 #                                  data.mi[data.mi$UserLanguage==langs.include[i],items.ps])
     data.aligned$vx <- t(F.vx)
-#    data.aligned$sps <- t(F.sps)
-#    data.aligned$identity <- t(F.id)
+    data.aligned$consp <- t(F.consp)
+    data.aligned$anti <- t(F.anti)
 #    data.aligned$resilience <- t(F.id)
 #    data.aligned$primary_stressor_avg <- t(F.ps)
   }else
@@ -115,12 +173,12 @@ for (i in 1:length(langs.include)){
     F.vx <- aligned.factor.scores(mod1.vx$lambda.aligned[i,],
                                    mod1.vx$nu.aligned[i,],
                                current[,vaxs])
-#    F.sps <- aligned.factor.scores(mod1.sps$lambda.aligned[i,],
-#                                   mod1.sps$nu.aligned[i,],
-#                                   current[,items.sps])
-#    F.id <- aligned.factor.scores(mod1.identity$lambda.aligned[i,],
-#                                  mod1.identity$nu.aligned[i,],
-#                                   current[,items.identity])
+    F.consp <- aligned.factor.scores(mod1.consp$lambda.aligned[i,],
+                                   mod1.consp$nu.aligned[i,],
+                                   current[,item.consp])
+    F.anti <- aligned.factor.scores(mod1.anti$lambda.aligned[i,],
+                                  mod1.anti$nu.aligned[i,],
+                                   current[,items.anti])
 ##    F.rs <- aligned.factor.scores(mod1.resilience$lambda.aligned[i,],
 #                                  mod1.resilience$nu.aligned[i,],
 #                                  current[,items.resilience])
@@ -128,8 +186,8 @@ for (i in 1:length(langs.include)){
 #                                  mod1.ps$nu.aligned[i,],
 #                                  current[,items.ps])
     current$vx <- t(F.vx)
-#    current$sps <- t(F.sps)
-#    current$identity <- t(F.id)
+    current$consp <- t(F.consp)
+    current$anti <- t(F.anti)
 #    current$resilience <- t(F.rs)
 #    current$primary_stressor_avg <- t(F.ps)
     data.aligned <- rbind(data.aligned,current)
@@ -168,7 +226,8 @@ save.image(file='Vaccine_aligned.RData')
 
 # alphas
 psych::alpha(data[,vaxs],check.keys=TRUE)
-psych::alpha(data[,items.sps],check.keys=TRUE)
-psych::alpha(data[,items.identity],check.keys=TRUE)
-psych::alpha(data[,items.resilience],check.keys=TRUE)
+psych::alpha(data[,item.consp],check.keys=TRUE)
+psych::alpha(data[,items.anti],check.keys=TRUE)
+
+psych::alpha(data[,items.anti],check.keys=TRUE)
 psych::alpha(data[,items.ps],check.keys=TRUE)
