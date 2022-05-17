d6107b46eb4e3d56ec1d84b9f914e3c8f0ce6e5f
Hyemin Han <hyemin.han@ua.edu>
2022-05-17 11:36:25 -0500

    mediation comparison updated

--- path_test.R
+++ path_test.R
@@ -65,5 +65,52 @@ med_result = brm(
   sample_prior ='yes', seed=1660415,prior=prior.coef
 )
 options(width = 2000)
-save(med_result, file = 'med_test.RData')
+summary(med_result)
 bayestestR::mediation(med_result)
+
+
+
+# Next. trust _ 7 -> anti -> vx
+# mediation model2
+data.filtered$anti<-scale(data.filtered$anti)
+model_mediator2 <- bf(anti~trust_7 + gender + education + work_location + age+
+                       SSS_faml+ relationship_status + (1+trust_7|residing_country))
+model_outcome2 <- bf(vx ~ anti+trust_7 + gender + education + work_location + age+
+                      SSS_faml+ relationship_status+ (1+trust_7+anti|residing_country))
+med_result2 = brm(
+  model_mediator2 + model_outcome2 + set_rescor(F),
+  data=data.filtered,
+  family = gaussian(),
+  cores=4,chains=4, save_pars = save_pars(all = T),
+  sample_prior ='yes', seed=1660415,prior=prior.coef
+)
+summary(med_result2)
+bayestestR::mediation(med_result2)
+
+
+
+# reverse test
+model_mediator.r <- bf(trust_6~consp + gender + education + work_location + age+
+                       SSS_faml+ relationship_status + (1+consp|residing_country))
+model_outcome.r <- bf(vx ~ consp+trust_6 + gender + education + work_location + age+
+                      SSS_faml+ relationship_status+ (1+trust_6+consp|residing_country))
+
+med_result.r = brm(
+  model_mediator.r + model_outcome.r + set_rescor(F),
+  data=data.filtered,
+  family = gaussian(),
+  cores=4,chains=4, save_pars = save_pars(all = T),
+  sample_prior ='yes', seed=1660415,prior=prior.coef
+)
+
+summary(med_result.r)
+bayestestR::mediation(med_result.r)
+
+# compare two models
+bf.comp.test <- bayes_factor(med_result,med_result.r)
+bf.comp.test.r <- bayes_factor(med_result.r,med_result, maxiter=10000)
+
+log(bf.comp.test.r$bf)
+
+
+save.image(file = 'med_test.RData')
